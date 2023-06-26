738f09c80b982b20e731c3f998b27aa27ea90aad
Rafael S Tavares <ragbriros@gmail.com>
2023-06-26 13:06:42 -0300

    Implementado o repositorio base em sql

---
+++ ControleFestasInfantis.Infra.Data.Sql/Compartilhado/RepositorioBaseEmSql.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleFestasInfantis.Infra.Data.Sql.Compartilhado
+{
+    internal class RepositorioBaseEmSql
+    {
+    }
+}
